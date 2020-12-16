62391c3b24209d24cbad79dc8893957b8da96b8b
tahiradmin <buat.loe@gmail.com>
2020-12-16 17:45:55 +0700

    Initial commit

---
+++ tentukan-nilai.php
@@ -0,0 +1,12 @@
+<?php
+function tentukan_nilai($number)
+{
+    //  kode disini
+}
+
+//TEST CASES
+echo tentukan_nilai(98); //Sangat Baik
+echo tentukan_nilai(76); //Baik
+echo tentukan_nilai(67); //Cukup
+echo tentukan_nilai(43); //Kurang
+?>
